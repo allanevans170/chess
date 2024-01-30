7908252034ec88b013b861c495c8827c4b60e412
allanevans170 <allanevans170@gmail.com>
2024-01-30 00:50:50 -0700

    changed board to 9x9

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,7 +7,7 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
-    private ChessPiece[][] boardSquares = new ChessPiece[8][8];
+    private ChessPiece[][] boardSquares = new ChessPiece[9][9];
 
     public ChessBoard() {
         boardSquares[1][1] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
@@ -21,7 +21,7 @@ public class ChessBoard {
         for (int i = 1; i <= 8; i++) {
             boardSquares[2][i] = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         }
-        
+
         boardSquares[8][1] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
         boardSquares[8][2] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
         boardSquares[8][3] = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
