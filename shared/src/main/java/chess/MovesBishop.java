8877f887156bec8094b66b4d469b7c7603c7dbaa
allanevans170 <allanevans170@gmail.com>
2024-01-30 12:06:29 -0700

    algorithm for bishop movement

--- shared/src/main/java/chess/MovesBishop.java
+++ shared/src/main/java/chess/MovesBishop.java
@@ -11,18 +11,58 @@ public class MovesBishop implements PieceMovesCalculator {
         int col = myPosition.getColumn();
 
         // up left
-        for (int i = row + 1, j = col - 1; i <= 8 && j >= 1; i++, j--) {
+        for (int i = row + 1, j = col - 1; i <= 8 || j >= 1; i++, j--) {
             ChessPosition pos = new ChessPosition(i, j);
             if (board.getPiece(pos) == null) {
                 bishopMoves.add(new ChessMove(myPosition, pos, null));
-            } else if (board.getPiece(pos).getTeamColor() == ChessGame.TeamColor.WHITE) {  // if enemy piece occupies the square, add it to the list and stop looking in this direction
+            } /*else if (board.getPiece(pos).getTeamColor() == ChessGame.TeamColor.WHITE) {  // if enemy piece occupies the square, add it to the list and stop looking in this direction
                 bishopMoves.add(new ChessMove(myPosition, pos, null));
                 break;
-            } else {
+            } */
+            else {
                 break;  // if a friendly piece occupies the square, stop looking in this direction
             }
-
         }
+        // up right
+        for (int i = row + 1, j = col + 1; i <= 8 || j <= 8; i++, j++) { // should this condition be OR?
+            ChessPosition pos = new ChessPosition(i, j);
+            if (board.getPiece(pos) == null) {
+                bishopMoves.add(new ChessMove(myPosition, pos, null));
+            } /*else if (board.getPiece(pos).getTeamColor() == ChessGame.TeamColor.WHITE) {  // if enemy piece occupies the square, add it to the list and stop looking in this direction
+                bishopMoves.add(new ChessMove(myPosition, pos, null));
+                break;
+            } */
+            else {
+                break;  // if a friendly piece occupies the square, stop looking in this direction
+            }
+        }
+        // down right
+        for (int i = row - 1, j = col + 1; i >= 1 || j <= 8; i--, j++) {
+            ChessPosition pos = new ChessPosition(i, j);
+            if (board.getPiece(pos) == null) {
+                bishopMoves.add(new ChessMove(myPosition, pos, null));
+            } /*else if (board.getPiece(pos).getTeamColor() == ChessGame.TeamColor.WHITE) {  // if enemy piece occupies the square, add it to the list and stop looking in this direction
+                bishopMoves.add(new ChessMove(myPosition, pos, null));
+                break;
+            } */
+            else {
+                break;  // if a friendly piece occupies the square, stop looking in this direction
+            }
+        }
+        // down left
+        for (int i = row - 1, j = col - 1; i >= 1 || j >= 1; i--, j--) {
+            ChessPosition pos = new ChessPosition(i, j);
+            if (board.getPiece(pos) == null) {
+                bishopMoves.add(new ChessMove(myPosition, pos, null));
+            } /*else if (board.getPiece(pos).getTeamColor() == ChessGame.TeamColor.WHITE) {  // if enemy piece occupies the square, add it to the list and stop looking in this direction
+                bishopMoves.add(new ChessMove(myPosition, pos, null));
+                break;
+            } */
+            else {
+                break;  // if a friendly piece occupies the square, stop looking in this direction
+            }
+        }
+
         return bishopMoves;
     }
 }
