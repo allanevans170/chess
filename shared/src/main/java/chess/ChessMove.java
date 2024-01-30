4e9c1e08d842fc5877d83eff1e4bbb3fd165f71f
allanevans170 <allanevans170@gmail.com>
2024-01-29 23:10:05 -0700

    filling out ChessMove, added toString method

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -8,22 +8,37 @@ package chess;
  */
 public class ChessMove {
 
+    private final ChessPosition startPosition;
+    private final ChessPosition endPosition;
+    private final ChessPiece.PieceType promotionPiece;
+
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+        this.startPosition=startPosition;
+        this.endPosition=endPosition;
+        this.promotionPiece=promotionPiece;
+    }
+
+    @Override
+    public String toString() {
+
+        return startPosition.toString() + " -> " + endPosition.toString();
+
+        // add promotion piece if not null!!!
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return startPosition;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return endPosition;
     }
 
     /**
@@ -33,6 +48,8 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return promotionPiece;
     }
 }
+
+
