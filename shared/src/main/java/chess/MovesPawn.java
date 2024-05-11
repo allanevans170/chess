a083a69b3cb93ce40c042d70bcb69dcb1e7c4d98
allanevans170 <allanevans170@gmail.com>
2024-05-11 12:25:25 -0600

    replace MovesPawn with programming exam version (it's cleaner)

--- shared/src/main/java/chess/MovesPawn.java
+++ shared/src/main/java/chess/MovesPawn.java
@@ -2,88 +2,85 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+
 public class MovesPawn extends MoveMaker {
-  Collection<ChessMove> pawnMoves = new ArrayList<>();
-
-  Directions[] whitePawnDirections = {Directions.UP, Directions.UP_RIGHT, Directions.UP_LEFT};
-  Directions[] blackPawnDirections = {Directions.DOWN, Directions.DOWN_RIGHT, Directions.DOWN_LEFT};
-  //Directions[] enPassantDirections = {Directions.LEFT, Directions.RIGHT};
-
-  ChessPiece.PieceType[] promotionPieces = {ChessPiece.PieceType.QUEEN, ChessPiece.PieceType.ROOK, ChessPiece.PieceType.BISHOP, ChessPiece.PieceType.KNIGHT};
-
-  // en passant ==  Directions.LEFT, Directions.RIGHT
-
+  private Collection<ChessMove> pawnMoves = new ArrayList<>();
+  private Directions[] whitePawnDirections = {
+          Directions.UP_LEFT,
+          Directions.UP,
+          Directions.UP_RIGHT
+  };
+  private Directions[] blackPawnDirections = {
+          Directions.DOWN_LEFT,
+          Directions.DOWN,
+          Directions.DOWN_RIGHT
+  };
+  private ChessPiece.PieceType[] PromotionPieces = {
+          ChessPiece.PieceType.QUEEN,
+          ChessPiece.PieceType.BISHOP,
+          ChessPiece.PieceType.KNIGHT,
+          ChessPiece.PieceType.ROOK
+  };
   @Override
   public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-    if (board.getPiece(myPosition).getTeamColor() == ChessGame.TeamColor.WHITE) {
-      pawnMoves.addAll(pawnMover(myPosition, whitePawnDirections, board));
+
+    ChessGame.TeamColor pawnColor = board.getPiece(myPosition).getTeamColor();
+    Directions[] pawnDirections;
+
+    if (pawnColor == ChessGame.TeamColor.WHITE) {
+      pawnDirections = whitePawnDirections;
     } else {
-      pawnMoves.addAll(pawnMover(myPosition, blackPawnDirections, board));
+      pawnDirections = blackPawnDirections;
     }
 
-    return pawnMoves;
-  }
+    for (Directions dir : pawnDirections) {
+      int row=myPosition.getRow();
+      int col=myPosition.getColumn();
+      int deltaRow = dir.getDeltaRow();
+      int deltaCol = dir.getDeltaCol();
 
-  public Collection<ChessMove> pawnMover(ChessPosition currPos, Directions[] direction, ChessBoard board) {
-    for (Directions dir : direction) {
-      int row=currPos.getRow();
-      int col=currPos.getColumn();
+      row+=deltaRow;
+      col+=deltaCol;
 
-      row+=dir.getDeltaRow();
-      col+=dir.getDeltaCol();
-
-      if (row < 1 || row > 8 || col < 1 || col > 8) {
+      if (row > 8 || row < 1 || col > 8 || col < 1 ) {
         continue;
       }
-
-      if (squareValidate(currPos, new ChessPosition(row, col), board) == 2) {    // empty square moves
-        if (dir == Directions.UP || dir == Directions.DOWN) {
-
-          pawnMoves.addAll(promoteOrNot(currPos, row, col));
-
-          if (pawnInitial(currPos, board, row, col) != null) {
-            pawnMoves.add(pawnInitial(currPos, board, row, col));
-          }
-
+      if (board.getPiece(new ChessPosition(row, col)) == null) {    // EMPTY SQUARE
+        if (dir == Directions.UP || dir == Directions.DOWN) {         // FORWARD MOVEMENT
+          pawnPromotionHelper(pawnColor, row, col, myPosition, new ChessPosition(row, col));
+        }
+        if (dir == Directions.UP && myPosition.getRow() == 2 && board.getPiece(new ChessPosition(row+1, col)) == null) {   // INITIAL WHITE
+          pawnMoves.add(new ChessMove(myPosition, new ChessPosition(row+1, col), null));
+        }
+        if (dir == Directions.DOWN && myPosition.getRow() == 7 && board.getPiece(new ChessPosition(row-1, col)) == null) { // INITIAL BLACK
+          pawnMoves.add(new ChessMove(myPosition, new ChessPosition(row-1, col), null));
         }
       }
-      else if (squareValidate(currPos, new ChessPosition(row, col), board) == 1) {    // enemy square
-        if (dir == Directions.UP_RIGHT || dir == Directions.UP_LEFT || dir == Directions.DOWN_RIGHT || dir == Directions.DOWN_LEFT) {
-          pawnMoves.addAll(promoteOrNot(currPos, row, col));
+      else if (board.getPiece(new ChessPosition(row, col)).getTeamColor() != pawnColor) {             // ENEMY SQUARE
+        if (pawnColor == ChessGame.TeamColor.BLACK && (dir == Directions.DOWN_LEFT || dir == Directions.DOWN_RIGHT)) {    // CAPTURE
+          pawnPromotionHelper(pawnColor, row, col, myPosition, new ChessPosition(row, col));
+        }
+        if (pawnColor == ChessGame.TeamColor.WHITE && (dir == Directions.UP_LEFT || dir == Directions.UP_RIGHT)) {        // CAPTURE
+          pawnPromotionHelper(pawnColor, row, col, myPosition, new ChessPosition(row, col));
         }
       }
     }
     return pawnMoves;
   }
 
-  public ChessMove pawnInitial (ChessPosition currPos, ChessBoard board, int row, int col) {
-    if (currPos.getRow() == 2 && board.getPiece(currPos).getTeamColor() == ChessGame.TeamColor.WHITE) {
-      if (squareValidate(currPos, new ChessPosition(row+1, col), board) == 2) {
-        return new ChessMove(currPos, new ChessPosition(row+1, col), null);
-      } else {
-        return null;
-      }
+  void pawnPromotionHelper(ChessGame.TeamColor pawnColor, int row, int col, ChessPosition myPosition, ChessPosition newPosition) {
+    if (newPosition.getRow() != 8 && pawnColor == ChessGame.TeamColor.WHITE) {
+      pawnMoves.add(new ChessMove(myPosition, new ChessPosition(row, col), null));
     }
-    else if (currPos.getRow() == 7 && board.getPiece(currPos).getTeamColor() == ChessGame.TeamColor.BLACK) {
-      if (squareValidate(currPos, new ChessPosition(row-1, col), board) == 2) {
-        return new ChessMove(currPos, new ChessPosition(row-1, col), null);
-      } else {
-        return null;
+    else if (newPosition.getRow() != 1 && pawnColor == ChessGame.TeamColor.BLACK) {
+      pawnMoves.add(new ChessMove(myPosition, new ChessPosition(row, col), null));
+    }
+    else {
+      for (ChessPiece.PieceType piece : PromotionPieces) {
+        pawnMoves.add(new ChessMove(myPosition, new ChessPosition(row, col), piece));
       }
-    } else {
-      return null;
     }
   }
 
-  public Collection<ChessMove> promoteOrNot(ChessPosition currPos, int row, int col) {
-    Collection <ChessMove> pawnPromotionMoves = new ArrayList<>();
-    if (row == 8 || row == 1) {
-      for (ChessPiece.PieceType pieceType : promotionPieces) {
-        pawnPromotionMoves.add(new ChessMove (currPos, new ChessPosition(row, col), pieceType));
-      }
-    } else {
-      pawnPromotionMoves.add(new ChessMove (currPos, new ChessPosition(row, col), null));
-    }
-    return pawnPromotionMoves;
-  }
+
 }
