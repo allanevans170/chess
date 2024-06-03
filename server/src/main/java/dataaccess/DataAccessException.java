0401034caf27346dcba89655bb5fc0a1248f0d69
allanevans170 <allanevans170@gmail.com>
2024-06-03 12:05:33 -0600

    use pet shop template to start SQLAuthDAO

--- server/src/main/java/dataaccess/DataAccessException.java
+++ server/src/main/java/dataaccess/DataAccessException.java
@@ -3,8 +3,18 @@ package dataaccess;
 /**
  * Indicates there was an error connecting to the database
  */
-public class DataAccessException extends Exception{
+public class DataAccessException extends Exception {
+    final private int statusCode;
+
     public DataAccessException(String message) {
         super(message);
+        statusCode = 500;
+    }
+    public DataAccessException(int statusCode, String message) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+    public int StatusCode() {
+        return statusCode;
     }
 }
--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -1,14 +1,24 @@
 package dataaccess;
 
+import com.google.gson.Gson;
 import model.AuthData;
-
 import java.util.Collection;
+
+import java.sql.*;
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class SQLAuthDAO implements AuthDAO {
 
+  public SQLAuthDAO() throws DataAccessException {
+    configureDatabase();
+  }
+
   @Override
   public AuthData createAuth(String username) throws DataAccessException {
-    return null;
+    var statement = "INSERT INTO auth (username, authToken) VALUES (?, ?)";
+    var json = new Gson().toJson(new AuthData(username));
+    var authToken = executeUpdate()
   }
 
   @Override
@@ -29,5 +39,31 @@ public class SQLAuthDAO implements AuthDAO {
   @Override
   public void deleteAllAuths() throws DataAccessException {
 
+  }
+
+  private final String[] createStatements = {
+          """
+            CREATE TABLE IF NOT EXISTS  auth (
+              `authToken` varchar(256) NOT NULL,
+              `username` varchar(256) NOT NULL,
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`authToken`),
+              
+              INDEX(type),
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+  };        // probably needs some work...
+  private void configureDatabase() throws DataAccessException {
+    DatabaseManager.createDatabase();
+    try (var conn = DatabaseManager.getConnection()) {
+      for (var statement : createStatements) {
+        try (var preparedStatement = conn.prepareStatement(statement)) {
+          preparedStatement.executeUpdate();
+        }
+      }
+    } catch (SQLException ex) {
+      throw new DataAccessException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+    }
   }
 }
