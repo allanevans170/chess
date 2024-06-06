3e3b86fd9b78ad21da0d987457a8d9d1bb838d7d
allanevans170 <allanevans170@gmail.com>
2024-06-05 19:01:33 -0600

    update memoryGameDAO to reflect how the database will do new gameIDs

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -7,11 +7,14 @@ import java.util.HashMap;
 public class MemoryGameDAO implements GameDAO {
 
   final private HashMap<Integer, GameData> games = new HashMap<>();
+  private int gameID = 1;
   @Override
   public GameData createGame(String gameName) throws DataAccessException {
     GameData game = new GameData(gameName);
     try {
+      game.setGameID(gameID);
       games.put(game.getGameID(), game);
+      gameID++;
     } catch (Exception e) {
       throw new DataAccessException("Game not created");
     }
