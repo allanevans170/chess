1c153721f2a7047c5c268f651f63bb6c8161af5a
allanevans170 <allanevans170@gmail.com>
2024-06-06 03:00:13 -0600

    set up for phase 5

---
+++ client/src/main/java/ui/BoardVisualizer.java
@@ -0,0 +1,45 @@
+package ui;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
+import static ui.EscapeSequences.*;
+
+public class BoardVisualizer {
+  private static final int BOARD_SIZE_IN_SQUARES = 8;
+  private static final int SQUARE_SIZE_IN_CHARS = 3;
+  //private static final int LINE_WIDTH_IN_CHARS = 1;
+  private static final String EMPTY = "   ";
+
+  public static void main(String[] args) {
+    var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+    out.print(ERASE_SCREEN);
+
+  }
+
+  private static void drawHeader(PrintStream out) {
+//    out.print(moveCursorToLocation(1, 1));
+//    out.print(SET_TEXT_BOLD + "  a b c d e f g h" + RESET_TEXT_BOLD);
+//    out.print(moveCursorToLocation(1, 2));
+//    out.print(SET_TEXT_BOLD + "  ┌─┬─┬─┬─┬─┬─┬─┬─┐" + RESET_TEXT_BOLD);
+  }
+
+  private static void drawFooter(PrintStream out) {
+//        out.print(moveCursorToLocation(1, 18));
+//        out.print(SET_TEXT_BOLD + "  └─┴─┴─┴─┴─┴─┴─┴─┘" + RESET_TEXT_BOLD);
+  }
+
+  private static void drawRow(PrintStream out, int row) {
+    // Draw the row number
+    // Draw squares, pieces, and separators
+
+//    out.print(moveCursorToLocation(1, 2 + row));
+//    out.print(SET_TEXT_BOLD + row + RESET_TEXT_BOLD);
+//    out.print(SET_TEXT_BOLD + " │" + RESET_TEXT_BOLD);
+//    for (int col = 0; col < BOARD_SIZE_IN_SQUARES; col++) {
+//      out.print(EMPTY);
+//      out.print(SET_TEXT_BOLD + "│" + RESET_TEXT_BOLD);
+//    }
+  }
+}
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -7,12 +7,19 @@ import server.Server;
 public class ServerFacadeTests {
 
     private static Server server;
+//    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+//        facade = new ServerFacade(port);
+    }
+
+    @BeforeEach
+    public void setup() {
+        // Clear the database
     }
 
     @AfterAll
@@ -24,6 +31,11 @@ public class ServerFacadeTests {
     @Test
     public void sampleTest() {
         Assertions.assertTrue(true);
-    }
+    } // REPLACE SAMPLE TEST WITH ACTUAL TESTS
 
+//    @Test
+//    void register() throws Exception {
+//        var authData = facade.register("player1", "password", "p1@email.com");
+//        assertTrue(authData.authToken().length() > 10);
+//    }
 }
