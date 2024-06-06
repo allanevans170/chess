9449a561e96e4e6b96dc63865a66ee65bf8d391c
allanevans170 <allanevans170@gmail.com>
2024-06-06 03:30:01 -0600

    add ChessClient class

---
+++ client/src/main/java/ChessClient.java
@@ -0,0 +1,18 @@
+public class ChessClient {
+
+  // pre-login UI
+
+  // - help
+  // - quit
+  // - login
+  // - register
+
+
+  // post-login UI
+  // - help
+  // - logout
+  // - create games
+  // - list games
+  // - play game
+  // - observe game
+}
